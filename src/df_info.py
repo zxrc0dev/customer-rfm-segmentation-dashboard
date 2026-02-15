8cb616199284cab10f72126f444c13c479e33110
zxrcodev <zxrcodev@icloud.com>
2026-02-15 16:10:30 +0100

    added df_info

---
+++ src/df_info.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+def df_info(df):
+    print(f"{'='*30} Head {'='*30}")
+    print(df.head())
+    print(f"{'='*30} Tail {'='*30}")
+    print(df.tail())
+    print(f"{'='*30} Shape {'='*30}")
+    print(df.shape)
+    print(f"{'='*30} Info {'='*30}")
+    print(df.info())
+    print(f"{'='*30} Columns {'='*30}")
+    print(df.columns)
+    print(f"{'='*30} Describe {'='*30}")
+    print(df.describe())
+    print(f"{'='*30} NaN {'='*30}")
+    print(df.isnull().sum())
+    print(f"{'='*30} Duplicates {'='*30}")
+    print(df.duplicated().sum())
